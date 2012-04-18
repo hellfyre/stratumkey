fe6d92f04ac95b30abeb532e2888b51f6e98d6a9
Matthias Uschok <dev@uschok.de>
2012-04-18 11:50:13 +0200

    Added some more master code
    At this point we should probably try out the communication

--- src/main_master.c
+++ src/main_master.c
@@ -12,6 +12,7 @@
 
 uint8_t secret[32];
 uint8_t challenge[32];
+uint8_t response[32];
 
 SIGNAL(SIG_INTERRUPT0) {
   owi_init(1<<WIREPIN);
@@ -28,6 +29,8 @@ int main(void) {
 void challenge_response_cycle() {
   owi_detectpresence(1<<WIREPIN);
   // TODO check return value of owi_detectpresence
+
+  // generate random challenge
   int i,j;
   for (i=0; i<8; i++) {
     unsigned long random_single = random();
@@ -37,7 +40,21 @@ void challenge_response_cycle() {
     }
   }
 
+  // send challenge
   for (i=0; i<32; i++) {
     owi_sendbyte(challenge[i], (1<<WIREPIN));
   }
+
+  // wait for the slave to process the challenge
+  // TODO count instructions on slave
+  _delay_us(20);
+
+  // receive response
+  for (i=0; i<32; i++) {
+    response[i] = owi_receivebyte(1<<WIREPIN);
+  }
+
+  // TODO check response
+  // like this: if $response = $(sha256($secret & $challenge [& $random_slave])
+  // $random_slave isn't actually needed, it's just plain paranoia
 }
