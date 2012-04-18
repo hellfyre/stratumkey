f438bff989a30d648f35dd9939baf09691e7cddc
Matthias Uschok <dev@uschok.de>
2012-04-19 01:23:41 +0200

    'Nother header file

--- src/main_master.c
+++ src/main_master.c
@@ -1,17 +1,10 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include <stdlib.h>
-
 #include <util/delay.h>
+#include <stdlib.h>
 
 #include <1wire/1wire.h>
 #include <avrcryptolib/sha256.h>
-
-#define WIREPIN PB0
-
-#define STATE_IDLE 0x00
-#define STATE_CHALLENGE 0x01
-#define STATE_RESPONSE 0x02
 
 uint8_t secret[32];
 uint8_t challenge[32];
@@ -20,7 +13,7 @@ uint8_t response[32];
 void challenge_response_cycle();
 
 SIGNAL(SIG_INTERRUPT0) {
-  owi_init(1<<WIREPIN);
+  owi_init(_BV(WIREPIN));
   challenge_response_cycle();
 }
 
@@ -32,7 +25,7 @@ int main(void) {
 }
 
 void challenge_response_cycle() {
-  owi_detectpresence(1<<WIREPIN);
+  owi_detectpresence(_BV(WIREPIN));
   // TODO check return value of owi_detectpresence
 
   // generate random challenge
@@ -47,7 +40,7 @@ void challenge_response_cycle() {
 
   // send challenge
   for (i=0; i<32; i++) {
-    owi_sendbyte(challenge[i], (1<<WIREPIN));
+    owi_sendbyte(challenge[i], _BV(1<<WIREPIN));
   }
 
   // wait for the slave to process the challenge
@@ -56,7 +49,7 @@ void challenge_response_cycle() {
 
   // receive response
   for (i=0; i<32; i++) {
-    response[i] = owi_receivebyte(1<<WIREPIN);
+    response[i] = owi_receivebyte(_BV(WIREPIN));
   }
 
   // TODO check response
---
+++ src/main_master.h
@@ -0,0 +1,16 @@
+#ifndef __MAIN_MASTER_H__
+#define __MAIN_MASTER_H__
+
+#ifdef attiny13
+#define WIREPIN PB0
+#endif
+
+#ifdef atmega8
+#define WIREPIN PD0
+#endif
+
+#define STATE_IDLE 0x00
+#define STATE_CHALLENGE 0x01
+#define STATE_RESPONSE 0x02
+
+#endif /*  __MAIN_MASTER_H__ */
