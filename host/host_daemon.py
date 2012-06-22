907287dc8fadeaced057d704074bedccbeeb1889
Matthias Uschok <dev@uschok.de>
2012-06-23 01:48:02 +0200

    Stuff

--- host/host_daemon.py
+++ host/host_daemon.py
@@ -4,32 +4,71 @@ import os
 import sys
 import daemon
 import lockfile
+import hashlib
+from threading import Thread
+
+import stratumkeyKeydb
+import stratumkeySerial
 
 import time
 
 def init():
-  # Set up serial connection
   port = '/dev/ttyUSB0'
-  ser = serial.Serial(port,9600,serial.EIGHTBITS,serial.PARITY_NONE,serial.STOPBITS_ONE,None,0,0,None)
-  ser.open()
-  ser.flushInput()
+  dbfile = 'somedb'
+
+  cipher = hashlib.sha256()
+
+  # Set up serial connection
+  # try
+  ser = stratumkeySerial.Serial(port)
 
   # Set up database
+  # try
+  db = stratumkeyKeydb.KeyDB(dbfile)
 
 def serial_loop():
+  while(True):
+    command = ser.readBytes(1)
+    if (command == '\x01'): # Key auth
+      id = ser.readID()
+
+      challenge = random.read(32)
+      ser.writeBytes(challenge)
+      response = ser.readBytes(32)
+
+      key = db.getKey(id)
+
+      if (key != None):
+        key_and_challenge = bytearray()
+        for i in range(0,32):
+          a = struct.unpack('B', key[i])[0]
+          b = struct.unpack('B', challenge[i])[0]
+          key_and_challenge.append( struct.pack('B', (a & b)) )
+
+        cipher.update(key_and_challenge)
+        key_hash = cipher.digest()
+
+        if (response == key_hash):
+          ser.openDoor()
+
+    elif (command == '\x02'): # Door bell
+      relayDoorBell()
 
 def main_loop():
+  serialThread = Thread(target=serial_loop)
+  serialThread.start()
+
   while(True):
-    outputfile.write("foobar\n")
     time.sleep(5)
 
 d = daemon.DaemonContext()
 d.working_directory='/home/matthias/working/personal_projects/stratumkey/host'
 d.pidfile=lockfile.FileLock('/home/matthias/working/personal_projects/stratumkey/host/stratumkey.pid')
 
-outputfile = open("foobar", 'w')
-outputfile.write("One\n")
-d.files_preserve=[outputfile]
+init()
+#outputfile = open("foobar", 'w')
+#outputfile.write("One\n")
+#d.files_preserve=[outputfile]
 
 with d:
   main_loop()
--- host/stratumkeySerial.py
+++ host/stratumkeySerial.py
@@ -3,57 +3,71 @@ import struct
 import sys
 import time
 import hashlib
-class key:
-	def uopen(port):
-		ser=serial.Serial(port,9600,serial.EIGHTBITS,serial.PARITY_NONE,serial.STOPBITS_ONE,None,0,0,None)
-		ser.open()
-		ser.flushInput()
-		return ser
-	def get_key(key_id):
-		#TODO get the key form some database	
-		a = 'db 96 88 dd f1 e8 d8 87 15 18 6e 5a 94 14 3f 63 5b 16 9b 38 e5 1c ea c7 b4 30 ef 99 27 27 5c a5'
-		somekey = bytearray.fromhex(a)
-		return somekey
-	def chall_and_key(chall,key):
-		return  key & chall
 
-	if __name__ ==  '__main__':
-		port="/dev/ttyUSB0"
-		inter="1"
-		file="/dev/hwrng"
+class Serial:
+  def __init__(self, port):
+    self.ser = serial.Serial(port, 9600, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE, None, 0, 0, None)
+    self.ser.open()
+    self.ser.flushInput()
+    self.cipher = hashlib.sha256()
 
-		try:
-			ser = uopen(port)
-		except:
-			print("no serial")
-			sys.exit(1)
+  def __del__(self):
+    self.ser.close()
 
-		f = open(file,"rb")
+  def readBytes(self, count):
+    return struct.unpack('B', self.ser.read(count))[0]
 
-		while(True):
-			chiper = hashlib.sha256()
-			d1=struct.unpack('B',ser.read(1))[0]
-			d2=struct.unpack('B',ser.read(1))[0]
-			key_id = (d1<<8) + d2
-			print("key_id: %d " % key_id)
-			key_a=get_key(key_id)
-			ck=bytearray()
-			for s in key_a:
-				byte=f.read(1)
-				challange = struct.unpack('B', byte)[0]
-				ck.append(challange & s)
-				ser.write(byte)
-			chiper.update(ck)
-			answer = bytearray()
-			for  i in range(32):
-				answer.append(ser.read(1))
-			data1=list(answer)
-			data2=list(chiper.digest())
-			if (chiper.digest() == answer):
-				print("mr. gorbatschow tear down this wall\n http://www.youtube.com/watch?v=rLBsOz8lPhI")
-				ser.write(struct.pack('B',0x99))
-			else:
-				print("du kommst hier net rein \n http://www.youtube.com/watch?v=jLhYIqiJlEA")
-				ser.write(struct.pack('B',0x00))
-			time.sleep(10)
+  def writeBytes(self, data):
+    return self.ser.write(buffer(data))
+
+  def readID():
+    d1=struct.unpack('B',readBytes(1))[0]
+    d2=struct.unpack('B',readBytes(1))[0]
+    return (d1<<8) + d2
+
+  def openDoor():
+    self.ser.writeBytes('\x10')
+
+#  def get_key(key_id):
+#    #TODO get the key form some database  
+#    a = 'db 96 88 dd f1 e8 d8 87 15 18 6e 5a 94 14 3f 63 5b 16 9b 38 e5 1c ea c7 b4 30 ef 99 27 27 5c a5'
+#    somekey = bytearray.fromhex(a)
+#    return somekey
+#  def chall_and_key(chall,key):
+#    return  key & chall
+#
+#  if __name__ ==  '__main__':
+#    port="/dev/ttyUSB0"
+#    inter="1"
+#    file="/dev/hwrng"
+#
+#    try:
+#      ser = uopen(port)
+#    except:
+#      print("no serial")
+#      sys.exit(1)
+#
+#    f = open(file,"rb")
+#
+#    while(True):
+#      key_a=get_key(key_id)
+#      ck=bytearray()
+#      for s in key_a:
+#        byte=f.read(1)
+#        challange = struct.unpack('B', byte)[0]
+#        ck.append(challange & s)
+#        ser.write(byte)
+#      chiper.update(ck)
+#      answer = bytearray()
+#      for  i in range(32):
+#        answer.append(ser.read(1))
+#      data1=list(answer)
+#      data2=list(chiper.digest())
+#      if (chiper.digest() == answer):
+#        print("mr. gorbatschow tear down this wall\n http://www.youtube.com/watch?v=rLBsOz8lPhI")
+#        ser.write(struct.pack('B',0x99))
+#      else:
+#        print("du kommst hier net rein \n http://www.youtube.com/watch?v=jLhYIqiJlEA")
+#        ser.write(struct.pack('B',0x00))
+#      time.sleep(10)
 
