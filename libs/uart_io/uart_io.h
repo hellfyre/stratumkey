566758cd8e02e179257bd0ddc1e47146f33427b9
Matthias Uschok <dev@uschok.de>
2014-03-14 23:19:11 +0100

    Reverted callback changes part II: UART IO

--- libs/uart_io/uart_io.h
+++ libs/uart_io/uart_io.h
@@ -5,17 +5,6 @@
 
 #include "uart_io/uart_device_defines.h"
 
-#define UART_CB_RECV_BUFFER_SIZE 64
-
-// USART data received callback
-typedef void (*uart_datarecv_cb_t)();
-extern uart_datarecv_cb_t uart_datarecv_callback; // one callback is plenty
-
-void uart_datarecv_cb_register(uart_datarecv_cb_t cb);
-void uart_datarecv_cb_unregister();
-uint8_t uart_ringbuffer_read(uint8_t *data, uint8_t len);
-void uart_ringbuffer_write(uint8_t data);
-
 void uart_init();
 void uart_receive(uint8_t *data, uint8_t length);
 void uart_transmit(uint8_t *data, uint8_t length);
